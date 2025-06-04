2fd53fb1a2fa0d6afba6026c5eb2b5bbbd0b51f0
ValentinaC <v.cifuentes04@ufromail.cl>
2025-06-03 20:20:55 -0400

    feat: se crea la barra de estado de forma inicial

---
+++ pompompurin-life/app/(tabs)/barra-de-estado.tsx
@@ -0,0 +1,43 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+
+export default function StatusTest() {
+  const [energy, setEnergy] = useState(40); // en porcentaje
+  const [hunger, setHunger] = useState(100);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.barContainer}>
+        <Text style={styles.title}>Energía: {energy}%</Text>
+      </View>
+      <View style={styles.barContainer}>
+        <Text style={styles.title}>Hambre: {hunger}%</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '15%',
+    flexDirection: 'row',
+    backgroundColor: '#fff8e1',
+    justifyContent:'center',
+    alignItems: 'center',
+    gap: '3%',
+    padding: '3%',
+  },
+  title: {
+    fontSize: 16,
+    marginBottom: 'auto',
+    fontWeight: 'bold',
+  },
+  barContainer: {
+    alignItems: 'center', // Centra el contenido
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+  }
+});

562d362c8efacf25e3d08259f959bea6ff9b8895
ValentinaC <v.cifuentes04@ufromail.cl>
2025-07-08 15:15:36 -0400

    feat: ahora pompompurin puede comer
    las imagenes son botones y puedes comer flanes presionando el boton inferior el cual llevara automaticamente al homeScreen

--- pompompurin-life/app/refrigerador.tsx
+++ pompompurin-life/app/refrigerador.tsx
@@ -1,34 +1,67 @@
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import { View, Text, StyleSheet,TouchableOpacity, Button, Image, Alert } from 'react-native';
 import VolverBoton from '@/components/VolverBoton';
 import { useRouter } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState } from 'react';
 
 export default function Refrigerador() {
   const router = useRouter();
+  const [comidaSeleccionada, setComidaSeleccionada] = useState(null); // ejemplo: 'flan'
 
+  const comerFlan = async () => {
+    try {
+      const storedHunger = await AsyncStorage.getItem('hunger');
+      const currentHunger = storedHunger ? parseInt(storedHunger) : 0;
+      const newHunger = Math.min(currentHunger + 15, 100);
+      await AsyncStorage.setItem('hunger', newHunger.toString());
+      Alert.alert('¡Ñam ñam!', 'Pompompurin comió flan 🍮');
+      router.push('/');
+    } catch (e) {
+      console.log('Error al aumentar hambre', e);
+    }
+  };
+
+  const botonTexto = comidaSeleccionada === 'flan' ? 'Comer flan' : 'Volver';
+  const onBotonPresionado = () => {
+    if (comidaSeleccionada === 'flan') {
+      comerFlan();
+    } else {
+      router.push('/');
+    }
+  };
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Refrigerador</Text>
             <View style={styles.refri}>
                 <View style={styles.estante}>
+                 <TouchableOpacity onPress={() => setComidaSeleccionada('flan')}
+                  style={styles.touchable}>
                   <Image
                     source={require('../assets/images/flan.png')}
-                    style={{ width: '50%', height: '100%', resizeMode: 'contain' }}
+                    style={styles.imagen}
                   />
+                 </TouchableOpacity>
                 </View>
                 <View style={styles.estante}>
+                 <TouchableOpacity onPress={() => setComidaSeleccionada('flan')}
+                 style={styles.touchable}>
                   <Image
                     source={require('../assets/images/flan.png')}
-                    style={{ width: '50%', height: '100%', resizeMode: 'contain' }}
+                    style={styles.imagen}
                   />
+                 </TouchableOpacity>
                 </View>
                 <View style={styles.estante}>
+                 <TouchableOpacity onPress={() => setComidaSeleccionada('flan')}
+                 style={styles.touchable}>
                   <Image
                     source={require('../assets/images/flan.png')}
-                    style={{ width: '50%', height: '100%', resizeMode: 'contain' }}
+                    style={styles.imagen}
                   />
+                 </TouchableOpacity>
                 </View>
             </View>
-            <VolverBoton onPress={() => router.push('/')}/>
+            <VolverBoton onPress={onBotonPresionado} title={botonTexto}/>
         </View>
     );
 }
@@ -64,5 +97,16 @@ const styles = StyleSheet.create({
         marginBottom: 20,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    touchable: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imagen: {
+        width: '60%',
+        height: '90%',
+        resizeMode: 'contain',
     },
 });
